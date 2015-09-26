fa5d999cc03c4f29aa22066b20cd02cf67758d32
Carolyn <carolyn.lee@yale.edu>
2015-09-26 15:34:06 -0700

    Added address form

---
+++ static/insureme.js
@@ -0,0 +1,40 @@
+////////////////////////////////////////////////////
+// Event Listeners
+
+$('.distance').on('input', syncDistanceValues);
+$('#search-btn').on('click', postLocation);
+
+
+////////////////////////////////////////////////////
+// Functions
+
+
+// Goople Maps Autocomplete 
+var address = document.getElementById('address');
+var options = {
+    types: ['address'] 
+};
+// Add autocomplete to the text boxes
+var autocomplete_address = new google.maps.places.Autocomplete(address, options);
+
+
+
+
+function syncDistanceValues(evt) {
+    var value = $(this).val();
+    $('.distance').val(value);
+}
+
+function postLocation() {
+
+    var locationData = {
+        address: $('#address').val(),
+        radius: $('#distance').val()
+    };
+
+    $.post('/locate', locationData, function(response) {
+
+    	// This is where we update the D3
+
+    });
+}

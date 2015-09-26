3ebdd087bbf2c5c88a71b42f1ee3d64b4f7e37e5
jessanettica <jessicalopez@Jessicas-MacBook-Pro-6.local>
2015-09-26 16:08:17 -0700

    API queries for doctor information

---
+++ betterdocinfo.py
@@ -0,0 +1,29 @@
+import requests
+import os
+
+
+def query_for_doctors():
+    """Show Doctor Information filtered by location"""
+    query_params = {'apikey': '###'}
+    endpoint = 'https://api.betterdoctor.com/2015-01-27/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=2&user_key=d9d6a34e907e4b42e1d4ad7a25f7998e'
+    response = requests.get(endpoint, params=query_params)
+    doctors = response.json()['data']
+
+    for doctor in doctors:
+        print "Working on doctor:"
+
+        first_name = doctor.get('profile').get('first_name')
+        last_name = doctor.get('profile').get('last_name')
+        title = doctor.get('profile').get('title')
+        gender = doctor.get('profile').get('gender')
+        # ratings = doctor.get('profile').get('ratings').get('rating')
+
+        # practice = doctor.get('practices').get('name')
+        # insurance_id = doctor.get('practices').get('insurance_uids')
+        # zipcode = doctor.get('practices').get('zip')
+
+        print first_name, last_name, title, gender
+
+if __name__ == "__main__":
+
+    query_for_doctors()

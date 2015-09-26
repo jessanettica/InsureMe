3cc84e362ab8737bae67d2512ae2ee31c4a46719
Carolyn <carolyn.lee@yale.edu>
2015-09-26 14:46:06 -0700

    Started HTML/CSS

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, session
+
+app = Flask(__name__)
+app.secret_key = "SECRET"
+
+###############################################
+# Routes
+
+@app.route('/')
+def home():
+	return render_template('base.html')
+
+###############################################
+# Main
+
+if __name__ == '__main__':
+	app.run(debug=True)

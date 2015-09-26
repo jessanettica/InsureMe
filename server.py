432b0dc60a773000b36ceb9184aad5677b4d49c3
Corey <jcshott@gmail.com>
2015-09-26 15:37:55 -0700

    added geocode

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, session
+import googlemaps
 
 app = Flask(__name__)
 app.secret_key = "SECRET"
@@ -9,6 +10,30 @@ app.secret_key = "SECRET"
 @app.route('/')
 def home():
 	return render_template('base.html')
+
+@app.route('/locate', methods=["POST"])
+def locate_user():
+	""" uses google maps geocode to get lat/lon of address"""
+
+	gmapKey = os.environ.get('Google_Maps_API_Key')
+	gmaps = googlemaps.Client(key=gmapKey)
+
+	address = request.form.get("address")
+	
+	# Geocoding and address
+	geocode_result = gmaps.geocode(address)
+
+	#parse the geocode result to get lat/long
+	geocode_info = geocode_result[0]
+	geometry = geocode_info.get('geometry')
+	location_data = geometry.get("location")
+	
+	latitude = location_data.get('lat')
+	longitude = location_data.get('lng')
+	radius = request.form.get("radius")
+	
+	return latitude, longitude, radius
+
 
 ###############################################
 # Main

2f23bb1fd92b1d23a043f4861871c34a1cc24388
jessanettica <jessicalopez@Jessicas-MacBook-Pro-6.local>
2015-09-26 17:32:00 -0700

    API queries for insurance information

--- betterdocinfo.py
+++ betterdocinfo.py
@@ -10,20 +10,34 @@ def query_for_doctors():
     doctors = response.json()['data']
 
     for doctor in doctors:
-        print "Working on doctor:"
 
         first_name = doctor.get('profile').get('first_name')
         last_name = doctor.get('profile').get('last_name')
         title = doctor.get('profile').get('title')
         gender = doctor.get('profile').get('gender')
         ratings = doctor.get('ratings')[0].get('rating')
+        specialties = doctor.get('specialties')[0].get('name')
 
         practice = doctor.get('practices')[0].get('name')
         insurance_id = doctor.get('practices')[0].get('insurance_uids')
         zipcode = doctor.get('practices')[0].get('visit_address').get('zip')
 
-        print first_name, last_name, title, gender, ratings, practice, zipcode
+    return
+
+def query_for_insurance():
+    """Show Insurance Information"""
+    query_params = {'apikey': '###'}
+    endpoint = 'https://api.betterdoctor.com/2015-01-27/insurances?skip=0&limit=10&user_key=d9d6a34e907e4b42e1d4ad7a25f7998e'
+    response = requests.get(endpoint, params=query_params)
+    insurance_plans = response.json()['data']
+
+    for insurance in insurance_plans:
+        insurance_id = insurance.get('plans')[0].get('uid')
+        insurance_name = insurance.get('plans')[0].get('name')
+        insurances[insurance_id]=insurance_name
+    return insurances
 
 if __name__ == "__main__":
 
     query_for_doctors()
+    query_for_insurance()

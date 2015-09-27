f8f09eeec942b7c7352d7fee18b518728ad0b69a
Carolyn <carolyn.lee@yale.edu>
2015-09-26 17:13:39 -0700

    5:13pm

--- static/insureme.js
+++ static/insureme.js
@@ -3,25 +3,14 @@
 
 $('.distance').on('input', syncDistanceValues);
 $('#search-btn').on('click', postLocation);
-
-
-////////////////////////////////////////////////////
-// Goople Maps Autocomplete 
-
-var address = document.getElementById('address');
-var options = {
-    types: ['address'] 
-};
-// Add autocomplete to the text boxes
-var autocomplete_address = new google.maps.places.Autocomplete(address, options);
-
+$(document).ready(autocomplete);
 
 ////////////////////////////////////////////////////
 // Functions
 
-function syncDistanceValues(evt) {
+function syncDistanceValues() {
     var value = $(this).val();
-    $('.distance').val(value);
+    $('span.distance').text(value);
 }
 
 function postLocation() {
@@ -49,4 +38,18 @@ function getD3() {
         error: function() { alert( "An Error Occurred in getD3" ); }
     });
 
+}
+
+
+////////////////////////////////////////////////////
+// Goople Maps Autocomplete 
+
+
+function autocomplete() {
+    var address = document.getElementById('address');
+    var options = {
+        types: ['address'] 
+    };
+    // Add autocomplete to the text boxes
+    var autocomplete_address = new google.maps.places.Autocomplete(address, options);
 }

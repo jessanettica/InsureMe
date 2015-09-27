3364bb0d4bff165e32b278fe3f75f0e737494f29
ammeurer <ammeurer8@gmail.com>
2015-09-27 12:04:51 -0700

    Added pagination of api calls

--- betterdocinfo.py
+++ betterdocinfo.py
@@ -43,86 +43,88 @@ def query_for_doctors(lat, lon, urange):
 
     #TODO: put cap on number we query in case we get area that has lots of docs.  Maybe cap at 
 
-    # query_params = {'user_key': 'd9d6a34e907e4b42e1d4ad7a25f7998e',
-    #                 'location': str(lat)+","+str(lon)+","+str(urange),
-    #                 'skip': '0',
-    #                 'limit': '1'
-    #                 }
-
-    # endpoint = 'https://api.betterdoctor.com/2015-01-27/doctors?'
-    # response = requests.get(endpoint, params=query_params)
-    # num_responses = response.json()['meta'].get('total')
-    # print "num_responses", num_responses
-    #TODO: limit number of loops - if num_responses > 1,000 then do 10 requests, 100 at a time. else. just loop through all per below
-    # for num in range(100):
-    #     print "num", num
     query_params = {'user_key': 'd9d6a34e907e4b42e1d4ad7a25f7998e',
-                'location': str(lat)+","+str(lon)+","+str(urange),
-                'skip': "0",
-                'limit': '100'
-                }
+                    'location': str(lat)+","+str(lon)+","+str(urange),
+                    'skip': '0',
+                    'limit': '1'
+                    }
 
     endpoint = 'https://api.betterdoctor.com/2015-01-27/doctors?'
     response = requests.get(endpoint, params=query_params)
+    num_responses = response.json()['meta'].get('total')
+    print "Total doctors returned: ", num_responses
+    if num_responses > 1000:
+        num_responses = 1000
+    #TODO: limit number of loops - if num_responses > 1,000 then do 10 requests, 100 at a time. else. just loop through all per below
+    for num in range(0, num_responses, 100):
+        print "num", num
+        query_params = {'user_key': 'd9d6a34e907e4b42e1d4ad7a25f7998e',
+                    'location': str(lat)+","+str(lon)+","+str(urange),
+                    'skip': str(num),
+                    'limit': '100'
+                    }
 
-    doctors = response.json()['data']
+        endpoint = 'https://api.betterdoctor.com/2015-01-27/doctors?'
+        response = requests.get(endpoint, params=query_params)
 
-    for doctor in doctors:
-        rating_0 = 0
-        rating_1 = 0
-        rating_2 = 0
-        rating_3 = 0
-        rating_4 = 0
-        rating_5 = 0
+        doctors = response.json()['data']
 
-        male = 0 #will be zero indx in sublist
-        female = 0 #will be 1 indx in sublist
+        for doctor in doctors:
+            rating_0 = 0
+            rating_1 = 0
+            rating_2 = 0
+            rating_3 = 0
+            rating_4 = 0
+            rating_5 = 0
 
-        first_name = doctor.get('profile').get('first_name')
-        last_name = doctor.get('profile').get('last_name')
-        title = doctor.get('profile').get('title')
-        gender = doctor.get('profile').get('gender')
+            male = 0 #will be zero indx in sublist
+            female = 0 #will be 1 indx in sublist
 
-        if gender == "male":
-            male += 1
-        if gender == "female":
-            female += 1
-        
-        if doctor.get('ratings'):
-            ratings = doctor.get('ratings')[0].get('rating')
-            if ratings < 2:
-                rating_1 += 1
-            elif ratings < 3:
-                rating_2 += 2
-            elif ratings < 4:
-                rating_3 += 1
-            elif ratings < 5:
-                rating_4 += 1
+            first_name = doctor.get('profile').get('first_name')
+            last_name = doctor.get('profile').get('last_name')
+            title = doctor.get('profile').get('title')
+            gender = doctor.get('profile').get('gender')
+
+            if gender == "male":
+                male += 1
+            if gender == "female":
+                female += 1
+            
+            if doctor.get('ratings'):
+                ratings = doctor.get('ratings')[0].get('rating')
+                if ratings < 2:
+                    rating_1 += 1
+                elif ratings < 3:
+                    rating_2 += 2
+                elif ratings < 4:
+                    rating_3 += 1
+                elif ratings < 5:
+                    rating_4 += 1
+                else:
+                    rating_5 += 1
             else:
-                rating_5 += 1
-        else:
-            rating_0 += 1
+                rating_0 += 1
 
-        specialties = doctor.get('specialties')[0].get('name') #returns list
+            specialties = doctor.get('specialties')[0].get('name') #returns list
 
-        practice = doctor.get('practices')[0].get('name')
-        
-        insurance_ids = doctor.get('practices')[0].get('insurance_uids') #returns list
-        zipcode = doctor.get('practices')[0].get('visit_address').get('zip')
+            practice = doctor.get('practices')[0].get('name')
+            
+            insurance_ids = doctor.get('practices')[0].get('insurance_uids') #returns list
+            zipcode = doctor.get('practices')[0].get('visit_address').get('zip')
 
-        #key: ins. ID; value = [#docs ins covers, [#rating 0, 1, 2, 3 4, 5]]
-        for i in insurance_ids:
-            doc_dict.setdefault(i, [0, [0, 0, 0, 0, 0, 0], [0, 0]])
-            doc_dict[i][0] += 1
-            doc_dict[i][1][0] += rating_0
-            doc_dict[i][1][1] += rating_1
-            doc_dict[i][1][2] += rating_2
-            doc_dict[i][1][3] += rating_3
-            doc_dict[i][1][4] += rating_4
-            doc_dict[i][1][5] += rating_5
-            doc_dict[i][2][0] += male
-            doc_dict[i][2][1] += female 
-        doc_count += 1
+            #key: ins. ID; value = [#docs ins covers, [#rating 0, 1, 2, 3 4, 5]]
+            for i in insurance_ids:
+                doc_dict.setdefault(i, [0, [0, 0, 0, 0, 0, 0], [0, 0]])
+                doc_dict[i][0] += 1
+                doc_dict[i][1][0] += rating_0
+                doc_dict[i][1][1] += rating_1
+                doc_dict[i][1][2] += rating_2
+                doc_dict[i][1][3] += rating_3
+                doc_dict[i][1][4] += rating_4
+                doc_dict[i][1][5] += rating_5
+                doc_dict[i][2][0] += male
+                doc_dict[i][2][1] += female 
+            doc_count += 1
     
     # limit_to_end = num_responses%100
     # query_params = {'user_key': 'd9d6a34e907e4b42e1d4ad7a25f7998e',
